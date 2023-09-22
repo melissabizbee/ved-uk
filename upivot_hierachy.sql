74ddcb0ddad25ef2c0c869926e93fd87ee4326c2
melissabizbee <88379511+melissabizbee@users.noreply.github.com>
2023-09-22 13:27:05 +0100

    initial commit

---
+++ upivot_hierachy.sql
@@ -0,0 +1,119 @@
+
+with unpivot_level_0 as (
+
+SELECT Level0CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level1CustomerKey, Level2CustomerKey, Level3CustomerKey,Level4CustomerKey,Level5CustomerKey,Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+
+),
+
+unpivot_level_1 as (
+
+SELECT Level1CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	--where Level0CustomerKey = 126641
+	UNPIVOT
+	(
+	  Value FOR Variable IN ( Level2CustomerKey, Level3CustomerKey,Level4CustomerKey,Level5CustomerKey,Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+unpivot_level_2 as (
+
+SELECT Level2CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	--where Level0CustomerKey = 126641
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level3CustomerKey,Level4CustomerKey,Level5CustomerKey,Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+unpivot_level_3 as (
+
+SELECT Level3CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level4CustomerKey,Level5CustomerKey,Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+unpivot_level_4 as (
+
+SELECT Level4CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level5CustomerKey,Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+unpivot_level_5 as (
+
+SELECT Level5CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level6CustomerKey,Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+unpivot_level_6 as (
+
+SELECT Level6CustomerKey as cusKey, Variable, Value
+	FROM BridgeCustomerHierarchy bch 
+	UNPIVOT
+	(
+	  Value FOR Variable IN (Level7CustomerKey)
+	) AS Unpvt
+
+),
+
+all_levels as (
+
+select * from unpivot_level_0
+union all
+select * from unpivot_level_1
+union all
+select * from unpivot_level_2
+union all
+select * from unpivot_level_3
+union all
+select * from unpivot_level_4
+union all
+select * from unpivot_level_5
+union all
+select * from unpivot_level_6
+
+
+
+
+),
+
+
+
+
+
+rename_vw as (
+
+select 
+distinct
+cusKey as CustomerKey_RLS, -- use this for rls
+SUBSTRING(variable,6,1) as Level,
+value as CustomerKey -- us this to join to customer in tableau
+from all_levels
+
+)
+
+select * from rename_vw
+ORDER BY LEVEL ASC
---
+++ vw_deposit_report.sql
@@ -0,0 +1,138 @@
+
+-- the purpose of this view is to bring in only what we need for the Tableau Deposit report. CTE's are used in case we need to do further aggregations, filters etc in order to optimise the data 
+
+-- this view can populate a table for Tableau to connect to if performance is stil an issue with read/ write. Due to the level of granularity required for Tableau i.e. days, bag number, we may have to do this.
+
+WITH customer AS (
+    SELECT 
+        C.[Customer Group],
+        C.[Customer Name],
+        C.[Customer Number],
+        C.[Customer Status],
+        C.[Customer Type],
+        C.CustomerKey,
+        C.[Store Reference]
+        
+    FROM [dbo].[Customer] C
+    where [Customer Deleted On] is null
+ ) ,
+
+mergeddeposit AS (
+    SELECT 
+        MD.[BagNumber],
+        MD.[BankAccountKey],
+        MD.[ContainerContentKey],
+        MD.CashCentreKey,
+        MD.[CashValue],
+        MD.[ChequeValue],
+        MD.[ChequeVolume],
+        MD.[CoinValue],
+        MD.[CoinVolume],
+        MD.CustomerKey,
+        MD.[CustomerSerialNo],
+        MD.[CustomerValue],
+        MD.[DifferenceValue],
+        MD.[DiscrepancyBankAccountKey],
+        MD.FactDepositId,
+        MD.[ForeignValue],
+        MD.[ForeignVolume],
+        MD.[ForgeryValue],
+        MD.[ForgeryVolume],
+        MD.HistoryDateKey,
+        MD.[NoteValue],
+        MD.[NoteVolume],
+        MD.[SealNumber],
+        MD.[TotalValue],
+        MD.[OriginalBagnumber],
+        CASE WHEN  CustomerValue <0 THEN TotalValue - CustomerValue END AS Shorts,
+        CASE WHEN  CustomerValue >0 THEN TotalValue - CustomerValue END AS Overs
+    FROM [dbo].[MergedDeposit] MD
+    WHERE MD.HistoryDateKey >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 13, 0)
+),
+
+bankaccount AS (
+    SELECT 
+        BA.[Account Number],
+        BA.BankAccountKey,
+        BA.[Sort code]
+    FROM [dbo].[BankAccount] BA
+),
+
+discrepancy_code AS ( 
+    SELECT 
+        D.[Discrepancy Category],
+        D.[Discrepancy Code],
+        D.[Discrepancy Description],
+        D.DiscrepancyKey
+    FROM dbo.Discrepancy D 
+
+),
+
+discrepancy_bridge AS (
+    SELECT 
+        BDD.DiscrepancyKey,
+        BDD.FactDepositID
+        
+    FROM dbo.BridgeDepositDiscrepancy BDD
+    WHERE BDD.HistoryDateKey >= DATEADD(MONTH, DATEDIFF(MONTH, 0, GETDATE()) - 13, 0)
+
+),
+
+cashcentre AS (
+    SELECT 
+        CC.[Cash Centre Description With Type],
+        CC.CashCentreKey
+    FROM [dbo].[CashCentre] CC
+),
+
+container AS (
+    SELECT 
+        CC.[GBP Value],
+        CC.ContainerContentKey
+    FROM [dbo].[ContainerContent] CC
+),
+
+final AS (
+    SELECT 
+        CUS.[Customer Number],
+        CUS.[Customer Name],
+        CUS.[Customer Type],
+        CUS.[Customer Status],
+        CUS.[Customer Group],
+        CUS.[Store Reference],
+        MD.[BagNumber],
+        MD.[CashValue],
+        MD.[ChequeValue],
+        MD.[ChequeVolume],
+        MD.[CoinValue],
+        MD.[CoinVolume],
+        MD.[CustomerSerialNo],
+        MD.[CustomerValue],
+        MD.[DifferenceValue],
+        MD.[DiscrepancyBankAccountKey],
+        MD.[ForeignValue],
+        MD.[ForeignVolume],
+        MD.[ForgeryValue],
+        MD.[ForgeryVolume],
+        MD.HistoryDateKey AS [History Date],
+        MD.[NoteValue],
+        MD.[NoteVolume],
+        MD.[SealNumber],
+        MD.[TotalValue],
+        BA.[Account Number],
+        BA.[Sort code],
+        DC.[Discrepancy Code],
+        DC.[Discrepancy Description],
+        DC.[Discrepancy Category],
+        CC.[Cash Centre Description With Type],
+        CONT.[GBP Value]
+    FROM  customer CUS 
+    INNER JOIN mergeddeposit MD ON CUS.CustomerKey = MD.CustomerKey
+    INNER JOIN bankaccount BA ON MD.BankAccountKey = BA.BankAccountKey
+    LEFT JOIN discrepancy_bridge DISC ON MD.FactDepositId = DISC.FactDepositId
+    LEFT JOIN discrepancy_code DC ON DISC.DiscrepancyKey = DC.DiscrepancyKey
+    INNER JOIN cashcentre CC ON MD.CashCentreKey = CC.CashCentreKey
+    INNER JOIN container CONT ON MD.ContainerContentKey = CONT.ContainerContentKey
+)
+
+SELECT * FROM final;
